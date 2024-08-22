1bf1c9463744fcd52c6c6cb43a904f056295632c
sivakumarsetti <sivakumarsettii@gmail.com>
2024-08-22 23:04:40 +0530

    second commit

--- src/Questions/Questions.jsx
+++ src/Questions/Questions.jsx
@@ -1,27 +1,37 @@
-"use client"
-import React, { useEffect, useState } from 'react'
-import questions from './Questions.json'
-import { Button } from '@/Button/Button'
-import styles from './Questions.module.css'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "@/Button/Button";
+import styles from "./Questions.module.css";
+import axios from 'axios'
 
 export const Questions = () => {
-  const[data,setData] = useState([])
-
-  function shuffle(array) {
+  const [data, setData] = useState([]);
+  const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-} 
-  useEffect(()=>{
-    setData(shuffle(questions))
-  },[])
-  
+};
+
+  const getQuestionsFromDb = async () => {
+    try {
+      const res = await axios.get("http://localhost:2025/question/get-data");
+      setData(shuffleArray(res?.data));
+    } catch (ex) {
+      setData([]);
+    } finally {
+    }
+  };
+
+  useEffect(() => {
+    getQuestionsFromDb();
+  }, []);
   return (
     <div className={styles.questions_cont}>
-      {data?.map((obj,index)=><Button key={index} {...obj}/>)}
+      {data?.map((obj, index) => (
+        <Button key={index} {...obj} />
+      ))}
     </div>
-  )
-}
-
+  );
+};
